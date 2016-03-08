674a83f961d162e1afe53bcb077beb8497044acb
Stephen Chapman <stevochapman@gmail.com>
2016-03-08 08:19:33 -0500

    update #1

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -8,3 +8,26 @@ $(document).foundation();
 
 // Init Highlight.js
 //hljs.initHighlightingOnLoad();
+
+// Parties contact form 
+var $contactForm = $('#parties-form');
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: '//formspree.io/stephen@restauranteve.com',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: 'json',
+		beforeSend: function() {
+			$contactForm.append('<div class="row"><div class="small-12 small-centered columns"><div class="callout" data-closable><h5>Sending</h5></div>');
+		},
+		success: function(data) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="row"><div class="small-12 small-centered columns"><div class="callout success" data-closable><h5>Message sent!</h5><p>We will be in touch with you shortly. Thank you for your enquiry.</p><button class="close-button" aria-label="Dismiss alert" type="button" data-close><span aria-hidden="true">&times;</span></button></div>');
+		},
+		error: function(err) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+		}
+	});
+});
