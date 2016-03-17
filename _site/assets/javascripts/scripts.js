554f2b33d09f48b79ae7f02125c62f3dc7d50a3d
Stephen Chapman <stevochapman@gmail.com>
2016-03-17 12:01:59 -0400

    improve cross browser form render

--- _site/assets/javascripts/scripts.js
+++ _site/assets/javascripts/scripts.js
@@ -10,8 +10,8 @@ $(document).foundation();
 //hljs.initHighlightingOnLoad();
 
 // Parties contact form 
-var $contactForm = $('#parties-form');
-$contactForm.submit(function(e) {
+ var $contactForm = $('#parties-form');
+ $contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
 		url: '//formspree.io/parties@societyfair.net',
@@ -31,3 +31,49 @@ $contactForm.submit(function(e) {
 		}
 	});
 });
+
+/** Parties contact form 
+
+// get all data in form and return object
+function getFormData() {
+  var elements = document.getElementById("gform").elements; // all form elements
+  var fields = Object.keys(elements).filter(function(k){
+    return k.length > 1 && elements[k].name && elements[k].name.length > 0 ;
+  });
+  var data = {};
+  fields.forEach(function(k){
+    data[k] = elements[k].value;
+  });
+  console.log(data);
+  return data;
+}
+
+function handleFormSubmit(event) {  // handles form submit withtout any jquery
+	event.preventDefault();           // we are submitting via xhr below
+	var data = getFormData();         // get the values submitted in the form
+	var url = event.target.action;  //
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', url);
+	// xhr.withCredentials = true;
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.onreadystatechange = function() {
+	    console.log( xhr.status, xhr.statusText )
+	    console.log(xhr.responseText);
+	    document.getElementById('gform').style.display = 'none'; // hide form
+	    document.getElementById('thankyou_message').style.display = 'block';
+	    return;
+	};
+	// url encode form data for sending as post data
+	var encoded = Object.keys(data).map(function(k) {
+	    return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+	}).join('&')
+	xhr.send(encoded);
+}
+function loaded() {
+  console.log('contact form submission handler loaded successfully');
+  // bind to the submit event of our form
+  var form = document.getElementById('gform');
+  form.addEventListener("submit", handleFormSubmit, false);
+};
+document.addEventListener('DOMContentLoaded', loaded, false);
+**/
